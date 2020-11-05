3c30290c3535dc802e7384dc4fb31e9976b31762
Dom <Sanek.s88@gmail.com>
2020-11-05 21:27:49 +0300

    05.11/4

--- assets/scripts/GameScene.js
+++ assets/scripts/GameScene.js
@@ -6,6 +6,12 @@ class GameScene extends Phaser.Scene{
         // 1. загрузить бэкгранд
         this.load.image('bg','assets/sprites/background.png');
         this.load.image('card','assets/sprites/card.png');
+        this.load.image('card1','assets/sprites/card1.png');
+        this.load.image('card2','assets/sprites/card2.png');
+        this.load.image('card3','assets/sprites/card3.png');
+        this.load.image('card4','assets/sprites/card4.png');
+        this.load.image('card5','assets/sprites/card5.png');
+
     };
     createBackground() {
         this.add.sprite(0,0,'bg').setOrigin(0,0);
@@ -13,9 +19,13 @@ class GameScene extends Phaser.Scene{
     createCard(){
         this.cards = [];
         let positons = this.getCardsPositions();
-        for (let positon of positons) {
-            this.cards.push(new Card(this,positon));
-        }
+        Phaser.Utils.Array.Shuffle(positons);
+
+        for (let value of config.cards) {
+            for(let i = 0; i < 2; i++){
+                this.cards.push(new Card(this,value,positons.pop()));
+            };
+        };
     };
     create(){
         this.createBackground();
